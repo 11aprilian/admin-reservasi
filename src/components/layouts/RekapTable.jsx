83af22a108aee40ed02851c89e3101407a4dd7d4
11aprilian <ihzaaprilian@gmail.com>
2023-06-12 21:27:21 +0700

    add print

--- src/components/layouts/RekapTable.jsx
+++ src/components/layouts/RekapTable.jsx
@@ -2,9 +2,11 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
-import { BsInfoCircle } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import moment from "moment";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 const RekapTable = () => {
   const [dataRekap, setDataRekap] = useState([]);
@@ -13,22 +15,25 @@ const RekapTable = () => {
   const [tglSelesai, setTglSelesai] = useState("");
   const [selected, setSelected] = useState("");
 
-  const [totalPendapatan, setTotalPendapatan] = useState("")
+  const [totalPendapatan, setTotalPendapatan] = useState("");
 
   const navigate = useNavigate();
 
   const fetchRekap = () => {
     axios
-      .get("http://localhost:3050/transaksi/rekap/" + tglMulai + "/" + tglSelesai)
+      .get(
+        "http://localhost:3050/transaksi/rekap/" + tglMulai + "/" + tglSelesai
+      )
       .then((result) => {
         const responseAPI = result.data;
         const totalHarga = responseAPI.data.reduce(
-          (totalAll, transaksi) => totalAll + parseInt(transaksi.total_pendapatan),
+          (totalAll, transaksi) =>
+            totalAll + parseInt(transaksi.total_pendapatan),
           0
         );
 
         setDataRekap(responseAPI.data);
-        setTotalPendapatan(totalHarga)
+        setTotalPendapatan(totalHarga);
       })
       .catch((err) => {
         console.log(err);
@@ -38,7 +43,7 @@ const RekapTable = () => {
   const handleStartDate = (date) => {
     const formattedDate = moment(date).format("YYYY-MM-DD");
     setTglMulai(formattedDate);
-    setSelected(date)
+    setSelected(date);
   };
 
   const handleEndDate = (date) => {
@@ -46,8 +51,86 @@ const RekapTable = () => {
     setTglSelesai(formattedDate);
   };
 
-  useEffect(() => {
-  }, []);
+  const downloadPdf = () => {
+    const input = document.getElementById("table-to-print");
+
+    html2canvas(input).then(() => {
+      const pdf = new jsPDF();
+      const today = new Date();
+      const dateString = today.toLocaleDateString();
+
+      pdf.setFont("Times New Roman");
+      pdf.setPage(1);
+
+      pdf.text(
+        "Laju Jaya Tour & Travel",
+        pdf.internal.pageSize.getWidth() / 2,
+        20,
+        { align: "center" }
+      );
+      pdf.text(
+        "Laporan Data Rekap Transaksi",
+        pdf.internal.pageSize.getWidth() / 2,
+        27,
+        { align: "center" }
+      );
+      pdf.text(
+        "RT.01/RW.03 Desa Tapan Kec. Kedungwaru, Kab. Tulungagung Jawa Timur 66229",
+        pdf.internal.pageSize.getWidth() / 2,
+        34,
+        { align: "center" },
+        pdf.setFontSize(12)
+      );
+
+      pdf.setLineWidth(1);
+      pdf.line(20, 38, 190, 38);
+      pdf.text(`Tanggal : ${dateString}`, 15, 48, pdf.setFontSize(12));
+      pdf.text(
+        `Data       : Rekap Travel ${tglMulai} / ${tglSelesai}`,
+        15,
+        55,
+        pdf.setFontSize(12)
+      );
+      pdf.text(
+        `Total Pendapatan : Rp. ${totalPendapatan}`,
+        140,
+        48,
+        pdf.setFontSize(12)
+      );
+
+      const tableY = 62;
+
+      const excludeColumns = [5];
+      const table = document.getElementById("table-to-print");
+      // Exclude columns from the table
+      excludeColumns.forEach((colIndex) => {
+        const th = table.getElementsByTagName("th")[colIndex];
+        const td = table.getElementsByTagName("td")[colIndex];
+        th.style.display = "none";
+        td.style.display = "none";
+      });
+
+      // Generate the table with excluded columns
+      pdf.autoTable({
+        html: table,
+        startY: tableY,
+      });
+
+      // Restore the excluded columns
+      excludeColumns.forEach((colIndex) => {
+        const th = table.getElementsByTagName("th")[colIndex];
+        const td = table.getElementsByTagName("td")[colIndex];
+        th.style.display = "";
+        td.style.display = "";
+      });
+
+      const blob = pdf.output("blob");
+      const url = URL.createObjectURL(blob);
+      window.open(url);
+    });
+  };
+
+  useEffect(() => {}, []);
 
   console.log("tglMulai", tglMulai);
   console.log("tglSelesai", tglSelesai);
@@ -65,29 +148,47 @@ const RekapTable = () => {
           />
         </div>
         <div className="col-md-4 mb-2">
-        <label className="small form-label">Tanggal Akhir :</label>
+          <label className="small form-label">Tanggal Akhir :</label>
           <div className="d-flex">
-          <DatePicker
-            className="form-control"
-            value={tglSelesai}
-            minDate={selected}
-            onChange={handleEndDate}
-          />
-          <button className="btn btn-outline-danger ms-4" onClick={fetchRekap}>Filter</button>
+            <DatePicker
+              className="form-control"
+              value={tglSelesai}
+              minDate={selected}
+              onChange={handleEndDate}
+            />
+            <button
+              className="btn btn-outline-danger ms-4"
+              onClick={fetchRekap}
+            >
+              Filter
+            </button>
           </div>
         </div>
         <div className="col-md-3 mb-2">
           <label className="small form-label">Total Pendapatan :</label>
-          <input className="form-control" placeholder={"Rp. " + totalPendapatan} disabled/>
+          <div className="d-flex">
+            <input
+              className="form-control"
+              placeholder={"Rp. " + totalPendapatan}
+              disabled
+            />
+            <button
+              className="btn btn-outline-danger ms-4"
+              onClick={downloadPdf}
+            >
+              Cetak
+            </button>
+          </div>
         </div>
       </div>
-      <table className="table">
+      <table className="table" id="table-to-print">
         <thead className="thead-dark bg-dark text-white">
           <tr>
             <th scope="col">Tanggal</th>
             <th scope="col">Nama Driver</th>
-            <th scope="col">Total Transaksi</th>
-            <th scope="col">Total Pendapatan</th>
+            <th scope="col">Jumlah Transaksi</th>
+            <th scope="col">Jumlah Perjalanan</th>
+            <th scope="col">Pendapatan</th>
             <th scope="col">Action</th>
           </tr>
         </thead>
@@ -95,10 +196,18 @@ const RekapTable = () => {
           {dataRekap.map((transaksi, index) => {
             return (
               <tr key={index}>
-                <td>{tglMulai}{" / "}{tglSelesai}</td>
+                <td>
+                  {tglMulai}
+                  {" / "}
+                  {tglSelesai}
+                </td>
                 <td>{transaksi.Jadwal_driver.Driver.nama}</td>
                 <td>{transaksi.jumlah_transaksi}</td>
-                <td>{"Rp. "}{transaksi.total_pendapatan}</td>
+                <td>{transaksi.jumlah_perjalanan}</td>
+                <td>
+                  {"Rp. "}
+                  {transaksi.total_pendapatan}
+                </td>
                 <td>
                   <Link
                     to={`/transaksi/laporan/${transaksi.Jadwal_driver.DriverId}/${tglMulai}/${tglSelesai}`}
--- src/pages/LaporanTransaksi.jsx
+++ src/pages/LaporanTransaksi.jsx
@@ -1,10 +1,10 @@
 import Navbar from "../components/layouts/Navbar";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import DatePicker from "react-datepicker";
-import { BsInfoCircle } from "react-icons/bs";
 import { useNavigate, useParams } from "react-router-dom";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 const LaporanTransaksi = () => {
   const { driverId, tglAwal, tglAkhir } = useParams();
@@ -12,7 +12,7 @@ const LaporanTransaksi = () => {
   const [dataLaporan, setDataLaporan] = useState([]);
   const [dataDriver, setDataDriver] = useState([]);
 
-  const [totalPendapatan, setTotalPendapatan] = useState("")
+  const [totalPendapatan, setTotalPendapatan] = useState("");
 
   const fetchLaporan = () => {
     axios
@@ -27,12 +27,12 @@ const LaporanTransaksi = () => {
       .then((result) => {
         const responseAPI = result.data;
         const totalHarga = responseAPI.data.reduce(
-            (totalAll, transaksi) => totalAll + transaksi.total,
-            0
-          );
+          (totalAll, transaksi) => totalAll + transaksi.total,
+          0
+        );
 
         setDataLaporan(responseAPI.data);
-        setTotalPendapatan(totalHarga)
+        setTotalPendapatan(totalHarga);
       })
       .catch((err) => {
         console.log(err);
@@ -41,8 +41,7 @@ const LaporanTransaksi = () => {
 
   const fetchDriver = () => {
     axios
-      .get(
-        "http://localhost:3050/driver/" + driverId)
+      .get("http://localhost:3050/driver/" + driverId)
       .then((result) => {
         const responseAPI = result.data;
 
@@ -51,6 +50,73 @@ const LaporanTransaksi = () => {
       .catch((err) => {
         console.log(err);
       });
+  };
+
+  const downloadPdf = () => {
+    const input = document.getElementById("table-to-print");
+
+    html2canvas(input).then((canvas) => {
+      const pdf = new jsPDF("l", "pt", "a4");
+      const today = new Date();
+      const dateString = today.toLocaleDateString();
+
+      pdf.setFont("Times New Roman");
+
+      pdf.setPage(1);
+
+      pdf.text(
+        "Laju Jaya Tour & Travel",
+        pdf.internal.pageSize.getWidth() / 2,
+        20,
+        { align: "center" }
+      );
+      pdf.text(
+        "Laporan Data Transaksi Driver",
+        pdf.internal.pageSize.getWidth() / 2,
+        40,
+        { align: "center" }
+      );
+      pdf.text(
+        "RT.01/RW.03 Desa Tapan Kec. Kedungwaru, Kab. Tulungagung Jawa Timur 66229",
+        pdf.internal.pageSize.getWidth() / 2,
+        55,
+        { align: "center" },
+        pdf.setFontSize(12)
+      );
+
+      pdf.setLineWidth(1);
+      pdf.line(20, 65, 820, 65);
+      pdf.text(`Tanggal : ${dateString}`, 20, 85, pdf.setFontSize(12));
+      pdf.text(
+        `Data       : Rincian Laporan Driver ${tglAwal} / ${tglAkhir}`,
+        20,
+        105,
+        pdf.setFontSize(12)
+      );
+      pdf.text(
+        `Nama Driver : ${dataDriver.nama}`,
+        670,
+        85,
+        pdf.setFontSize(12)
+      );
+      pdf.text(
+        `Total Pendapatan : Rp. ${totalPendapatan}`,
+        670,
+        105,
+        pdf.setFontSize(12)
+      );
+
+      const tableY = 125;
+
+      pdf.autoTable({
+        html: "#table-to-print",
+        startY: tableY,
+      });
+
+      const blob = pdf.output("blob");
+      const url = URL.createObjectURL(blob);
+      window.open(url);
+    });
   };
 
   useEffect(() => {
@@ -62,51 +128,81 @@ const LaporanTransaksi = () => {
     <div>
       <Navbar />
       <div className="container-fluid table-responsive-sm mt-3">
-      <div className="row mb-3">
-        <div className="col-md-3 mb-2">
-          <label className="small form-label">Nama Driver :</label>
-          <input className="form-control" placeholder={dataDriver.nama} disabled/>
+        <div className="row mb-3">
+          <div className="col-md-3 mb-2">
+            <label className="small form-label">Nama Driver :</label>
+            <input
+              className="form-control"
+              placeholder={dataDriver.nama}
+              disabled
+            />
+          </div>
+          <div className="col-md-3 mb-2">
+            <label className="small form-label">Dari Tanggal :</label>
+            <input className="form-control" placeholder={tglAwal} disabled />
+          </div>
+          <div className="col-md-3 mb-2">
+            <label className="small form-label">Sampai Tanggal :</label>
+            <input className="form-control" placeholder={tglAkhir} disabled />
+          </div>
+          <div className="col-md-3 mb-2">
+            <label className="small form-label">Total Pendapatan :</label>
+            <div className="d-flex">
+              <input
+                className="form-control"
+                placeholder={"Rp. " + totalPendapatan}
+                disabled
+              />
+              <button
+                className="btn btn-outline-danger ms-4"
+                onClick={downloadPdf}
+              >
+                Cetak
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="col-md-3 mb-2">
-          <label className="small form-label">Dari Tanggal :</label>
-          <input className="form-control" placeholder={tglAwal} disabled/>
-        </div>
-        <div className="col-md-3 mb-2">
-          <label className="small form-label">Sampai Tanggal :</label>
-          <input className="form-control" placeholder={tglAkhir} disabled/>
-        </div>
-        <div className="col-md-3 mb-2">
-          <label className="small form-label">Total Pendapatan :</label>
-          <input className="form-control" placeholder={"Rp. "+totalPendapatan} disabled/>
-        </div>
-        </div>
-        <table className="table">
+        <table
+          className="table small table-bordered border-dark"
+          id="table-to-print"
+        >
           <thead className="thead-dark bg-dark text-white">
             <tr>
+              <th scope="col">No</th>
               <th scope="col">ID</th>
-              <th scope="col">Pelanggan</th>
+              <th scope="col">Nama Pelanggan</th>
+              <th scope="col">Nama Penumpang</th>
               <th scope="col">Alamat</th>
               <th scope="col">No Hp</th>
-              <th scope="col">Driver</th>
+              {/* <th scope="col">Driver</th> */}
               <th scope="col">Rute</th>
               <th scope="col">Waktu Transaksi</th>
               <th scope="col">Waktu Travel</th>
-              <th scope="col">Uang Masuk</th>
+              <th scope="col">Pendapatan</th>
             </tr>
           </thead>
           <tbody>
             {dataLaporan.map((transaksi, index) => {
               return (
                 <tr key={transaksi.id}>
+                  <td>{index + 1}</td>
                   <td>{transaksi.id}</td>
                   <td>{transaksi.User.username}</td>
+                  <td>{transaksi.nama}</td>
                   <td>{transaksi.alamat}</td>
                   <td>{transaksi.no_hp}</td>
-                  <td>{transaksi.Jadwal_driver.Driver.nama}</td>
+                  {/* <td>{transaksi.Jadwal_driver.Driver.nama}</td> */}
                   <td>{transaksi.Jadwal_driver.Rute.arah}</td>
-                  <td>{transaksi.updatedAt}</td>
-                  <td>{transaksi.tanggal}{", "}{transaksi.Jadwal_driver.Jam.jam}</td>
-                  <td>{"Rp. "}{transaksi.total}</td>
+                  <td>{transaksi.updatedAt.slice(0, 10)}</td>
+                  <td>
+                    {transaksi.tanggal}
+                    {", "}
+                    {transaksi.Jadwal_driver.Jam.jam}
+                  </td>
+                  <td>
+                    {"Rp. "}
+                    {transaksi.total}
+                  </td>
                 </tr>
               );
             })}
